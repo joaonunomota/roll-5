0c9bd1c107797618e0e6f131a022409d887027cb
João Nuno Mota <jnuno.carrilho@gmail.com>
2020-05-25 12:39:49 +0100

    Add normalize css and add Minecraftia font

--- src/App.vue
+++ src/App.vue
@@ -86,4 +86,15 @@ export default {
   }
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+@import "~normalize.css";
+* {
+  font-family: "MinecraftiaRegular";
+  color: inherit;
+  font-size: 14px;
+}
+body {
+  background-color: #008148;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23034732' fill-opacity='0.4' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
+}
+</style>
